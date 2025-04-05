b82999c281022f03fae7bda305c78d5e74b6f0d3
Benjamin Zuckerman <benjamin.zuckerman@nhs.net>
2025-04-04 17:27:25 -0700

    test script which incorporates format_pqtl_ukbbb() + run_mr()

--- inst/scripts/test_script_cd40_sjd_using_full_gwas_data.R
+++ inst/scripts/test_script_cd40_sjd_using_full_gwas_data.R
@@ -1,28 +1,32 @@
-#sjd <- data.table::fread("/Users/benzuckerman/Library/Mobile Documents/com~apple~CloudDocs/Research/mr_work/data/sjogrens_6098_34928.txt")
+sjd <- data.table::fread("/Users/benzuckerman/Library/Mobile Documents/com~apple~CloudDocs/Research/mr_work/data/sjogrens_6098_34928.txt")
 
-#cd40 <- data.table::fread("/Users/benzuckerman/Downloads/cd40_rsids_olink_h37.csv")
+sjd <- as.data.frame(sjd)
+sjd <- sjd |>
+  dplyr::mutate(phenotype == "SjD")
 
-#sjd_1 <- sjd |>
-#  dplyr::mutate(chrom = chr,
-#                rsids = SNP,
-#                alt = effect_allele,
-#                ref = other_allele,
-#                sebeta = se,
-#                af_alt = eaf) |>
-#  dplyr::mutate(phenotype = "SjD")
+sjd <- TwoSampleMR::format_data(
+  sjd,
+  type = "outcome",
+  phenotype_col = "phenotype"
+  header = "TRUE",
+  snp_col = "rsids",
+  effect_allele_col = "effect_allele",
+  other_allele_col = "other_allele",
+  eaf_col = "eaf",
+  beta_col = "beta",
+  se_col = "se",
+  pval_col = "pval"
+)
 
-#sjd_1 <- as.data.frame(sjd_1)
-#cd40 <- as.data.frame(cd40)
+cd40 <- format_pqtl_ukbppp(ukbppp = "data/CD40_data_50.csv",
+                   ukbppp_rsid = "data/CHR20_data_50.csv",
+                   pqtl_assay = "CD40")
 
 run_mr(exposure = cd40,
        exposure_id = "CD40",
-       outcome = sjd_1,
+       outcome = sjd,
        outcome_id = "SjD",
        instrument_region = list(chromosome = 20, start = 44746911, end = 44758502),
        pval_thresh = 5e-06,
        rsq_thresh = 0.1,
        bfile = "/Users/benzuckerman/LD_folder/g1000_eur")
-
-test <- format_pqtl_ukbppp(ukbppp = "data/CD40_data_50.csv",
-                   ukbppp_rsid = "data/CHR20_data_50.csv",
-                   pqtl_assay = "CD40")
