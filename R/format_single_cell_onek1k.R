f2bd67d6f3edbfe766e708e34a8783032cc64de7
Benjamin Zuckerman <benjamin.zuckerman@nhs.net>
2025-09-15 20:26:55 +0100

    roxygen

--- R/format_single_cell_onek1k.R
+++ R/format_single_cell_onek1k.R
@@ -1,12 +1,38 @@
-#' Formats single-cell RNA eQTL data frame
+#' Format single-cell RNA eQTL data for analysis
 #'
-#' Purpose of this script of functions are to create the pathway to
-#' the eQTL data and then format the eQTL data ready for analysis
-#' Make sure list of cell_types are created/called from the mapping file
-
+#' This function reads in eQTL data for a specified cell type using a provided mapping file
+#' and formats the data frame to prepare it for downstream analysis.
+#' The function expects the eQTL data directory path and uses `TwoSampleMR::format_data`
+#' to structure the data in a standardized format.
+#'
+#' @param mapping_file Character. File path to the mapping file containing eQTL data paths.
+#' @param cell_type Character. Name of the cell type to subset the eQTL data.
+#' @param eqtl_dir Character or data.frame. Either the path to the eQTL data directory or a pre-loaded data frame.
+#'
+#' @return A formatted data frame suitable for Mendelian randomization analysis.
+#'
+#' @details
+#' The function reads eQTL data files using `data.table::fread()`, computes standard errors,
+#' renames and selects relevant columns, and formats the dataset using `TwoSampleMR::format_data()`.
+#' Ensure that the list of cell types is consistent with the mapping file.
+#'
+#' @importFrom data.table fread
+#' @importFrom dplyr mutate select
+#' @importFrom purrr map
+#' @importFrom TwoSampleMR format_data
+#'
+#' @examples
+#' \dontrun{
+#' mapping_file <- "path/to/mapping_file.txt"
+#' cell_type <- "T_cells"
+#' eqtl_dir <- "path/to/eqtl_data/"
+#' formatted_data <- format_single_cell_onek1k(mapping_file, cell_type, eqtl_dir)
+#' }
+#'
+#' @export
 format_single_cell_onek1k <- function(mapping_file,
-                               cell_type,
-                               eqtl_dir){
+                                      cell_type,
+                                      eqtl_dir){
   #' Read in eQTL data for individual cell type using mapping_file pathway
   if (is.character(eqtl_dir)) {
     # Read in files using data.table::fread()
